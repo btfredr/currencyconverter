1a0f42c6470da35e2f2ae3945ed1c854232271dd
Fredrik Fordelsen <fredrikf2000@gmail.com>
2025-09-22 12:40:46 +0200

    project init

---
+++ src/main/java/CurrencyConverter.java
@@ -0,0 +1,18 @@
+import javafx.application.Application;
+import javafx.concurrent.Task;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.URI;
+
+public class CurrencyConverter {
+
+    @Override
+    public void start(Stage stage) {
+        
+    }
+}
